02931f2485ffebca46450a8b541bd56165e2fdff
fefeluis <fefeluis1441@gmail.com>
2022-02-21 19:08:40 -0300

    finished

--- Scrolling Effect/script.js
+++ Scrolling Effect/script.js
@@ -10,4 +10,7 @@ window.addEventListener ('scroll', function(){
     stars.style.left = value * 0.25 + 'px';
     moon.style.top = value * 1.05 + 'px'
     mountains_behind.style.top = value * 0.05 + 'px'
+    mountains_front.style.top = value * 0 + 'px'
+    text.style.marginRight = value * 4 + 'px'
+    btn.style.marginTop = value * 1.5 + 'px'
 })
